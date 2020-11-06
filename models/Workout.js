c309848d7742e70db0c732930ca9c9c156fbdb89
jsp <jonsp14@gmail.com>
2020-11-06 15:23:58 -0800

    issues with update, request displaying

--- models/Workout.js
+++ models/Workout.js
@@ -5,8 +5,8 @@ const WorkoutSchema = new Schema({
     day: { type: Date, default: Date.now },
     exercises: [
         {
-            type: { type: String },
-            name: { type: String },
+            type: { type: String, trim: true },
+            name: { type: String, trim: true },
             duration: { type: Number },
             weight: { type: Number },
             reps: { type: Number },
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 // require models
 const db = require("../models/Workout.js");
+const mongojs = require("../node_modules/mongodb");
 
 module.exports = function (app) {
 
@@ -14,18 +15,28 @@ module.exports = function (app) {
     });
 
     // add exercise
-    app.get("/api/workouts/:id", (req, res) => {
-
+    app.put("/api/workouts/:id", (req, res) => {
+        console.log("req", req);
+        console.log("id", req.params.id);
+        db.update({ _id: req.params.id },
+            {
+                $push: { exercises: req.body }
+            }).then(workout => res.json(workout));
     });
 
     // create workout
-    app.post("/api/workouts", ({ body }, res) => {
-        
+    app.post("/api/workouts", (req, res) => {
+        db.create()
+            .then(workout => {
+                res.json(workout);
+            }).catch(err => {
+                res.json(err);
+            });
     });
 
     // get workouts in range
     app.post("/api/workouts/range", (req, res) => {
-       
+
     });
 
 }
